4fd060b60c7841270b3b43e4a733f97f36b1a5ff
SaurabhRaje <saurabh.r@live.com>
2014-05-13 13:09:49 +0530

    CCD Banner for Go To Store
    1. Image and HTML file for Go To Store with CCD Banner.

---
+++ sendtostore.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8"/>
+    <title>Transition Page</title>
+    <style type="text/css">
+      body {
+        margin: 0;
+        font-family: Arial, sans-serif;
+        background-color: #fff;
+        color: #333;
+      }
+      .gotostoreWrapper {
+        margin: auto;
+        position: fixed;
+        top: 0;
+        right: 0;
+        bottom: 0;
+        left: 0;
+        height: 550px;
+        text-align: center;
+      }
+      .msp-logo img {
+        width: 175px;
+      }
+      .title {
+        margin-top: 20px;
+        font-size: 18px;
+      }
+      .title .red {
+        color: #c00;
+      }
+      .status {
+        margin-top: 90px;
+        font-size: 18px;
+        font-weight: bold;
+      }
+      .status img {
+        margin-left: 5px;
+        vertical-align: middle;
+      }
+      .loading {
+        margin-top: 25px;
+      }
+      .store-link {
+        text-decoration: none;
+        font-size: 12px;
+        color: #666;
+      }
+      .store-link:hover {
+        color: #999;
+      }
+      .offer {
+        margin-top: 90px;
+      }
+    </style>
+  </head>
+  <body>
+    <div class="gotostoreWrapper">
+      <div class="msp-logo">
+        <img src="/images/logo_b.png" alt="MySmartPrice"/>
+      </div>
+      <div class="title">10 million+ products. 100+ online stores. <span class="red">find more. save more.</span></div>
+      <div class="status">
+        On your way to
+        <a class="store-logo" href="#">
+          <img src="http://c223968.r68.cf1.rackcdn.com/amazon_store.png" alt="MySmartPrice"/>
+        </a>
+      </div>
+      <div class="loading">
+        <img src="/images/loading.gif" alt="Loading..."/>
+      </div>
+      <div class="message">
+        <a class="store-link" href="#">(if taking long, click here)</a>
+      </div>
+      <div class="offer">
+        <a class="offer-link" href="#" target="_blank">
+          <img src="/images/ccd-banner.png" alt="Loading..."/>
+        </a>
+      </div>
+    </div>
+  </body>
+</html>
