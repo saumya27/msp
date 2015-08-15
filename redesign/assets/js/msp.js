138acbcce60063cfb326799ce318a50ec19ebe53
Ankur Agarwal <er.ankurag@yahoo.in>
2015-08-15 18:16:11 +0530

    Clear button on color filter

--- redesign/assets/js/msp.js
+++ redesign/assets/js/msp.js
@@ -1231,7 +1231,7 @@ function bindAutoComplete() {
                 var term = this.term.split(' ')
                     .join('|'),
                     re = new RegExp("\\b(" + term + ")", "gi"),
-                    tempval = item.value.replace(re, "<b>$1</b>");
+                    tempval = item.value.replace(re, "<strong>$1</strong>");
                 if (item.subcategory !== "") tempval += " in <span style='color:#c00;font-weight:bold;'>" + item.subcategory + "</span>";
                 return $("<li></li>")
                     .data("item.autocomplete", item)
