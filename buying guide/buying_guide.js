fa99cfd37433967b08e7841b1a6259b6bc01cd50
Saumya Aggarwal <saumya@mysmartprice.com>
2015-09-03 18:00:43 +0530

    dev of BG

--- buying guide/buying_guide.js
+++ buying guide/buying_guide.js
@@ -1,5 +1,35 @@
+var allowed = true;
+
+$('.rvw-indx').on('click',function() {
+  $('.rvw-indx').removeClass('selected');
+    $(this).addClass('selected');
+    var section_id = $(this).attr('data-target-section');
+  var target = $('#' + section_id), offset = 50;
+  allowed = false;
+  
+  if($(this).attr('data-target-section') == "overview-sctn"){
+    offset = 200;
+  }
+  
+  $("body, html").animate({scrollTop: target.position().top - offset}, 700);
+  setTimeout(function(){
+    allowed = true;
+  },750);
+});
+
+
 $(document).ready(function(){
     $(".faq__sub-sctn").find(".expnd-hd:first").find('.expand-collapse').click();
+
+    $(".js-sldr").each(function(e){
+        elementSlider.init(this);
+    });
+    $doc.on("click", ".js-sldr__prvs", function() {
+        elementSlider.slide(this,"left");
+    });
+    $doc.on("click", ".js-sldr__next", function() {
+        elementSlider.slide(this, "right");
+    });
 });
 
 $(".expand-collapse").on('click', toggleSlider);
@@ -84,3 +114,113 @@ $(".cntxt-link-item__scr").each( function(){
         if (score >= min && score <= max) { return score; }
         else { return !score; }
      }
+
+
+$(window).scroll(function() {
+  $window = $(window);
+  $('.rvw__rght-pnl .sctn').each(function(){
+    var sectionID = this.id ; 
+
+    if ($window.scrollTop() > $(this).position().top -200 && allowed) {
+      $('.rvw-indx').removeClass('selected');
+        $('[data-target-section="'+ sectionID +'"]').addClass('selected');
+    }
+  });
+});
+
+elementSlider =  {
+    "init" : function(slider) {
+        var slideItem = $(slider).data("slideitem"),
+            slideItemWrapper = $(slider).data("slideitemwrapper");
+
+            if(!slideItem || !slideItemWrapper) return;
+            if($(slider).find("." + slideItemWrapper).hasClass("js-sldr-item-wrpr1")) return;
+
+        var $elements = $(slider).find("." + slideItem),
+            countCurrItems = Math.floor($(slider).find("." + slideItemWrapper).eq(0).width() / $elements.eq(0).outerWidth(true));
+
+        if($elements.length > countCurrItems) {
+            $(slider).find("." + slideItemWrapper).addClass("js-sldr-item-wrpr1").
+                                                          wrapInner("<div class='js-sldr-item-wrpr'></div>");
+
+            $(slider).find("." + slideItem).eq(0).addClass("js-sldr-crnt");
+            $(slider).find(".js-sldr__prvs").addClass("js-sldr__dsbl-btn").show();
+            $(slider).find(".js-sldr__next").show();
+        }       
+    },
+
+    "slide" : function(element, direction) {
+         var $slider = $(element).closest(".js-sldr"),
+            $elementWrapper =  $slider.find(".js-sldr-item-wrpr"),
+            slideItem = $slider.data("slideitem"),
+            $elements = $elementWrapper.find("." + slideItem),
+            $currentElement =  $elements.filter(".js-sldr-crnt"),
+            $startElement = null,
+
+            countCurrItems = Math.floor($elementWrapper.width() / $elements.eq(0).outerWidth(true)),
+            countRightItems = $elements.length - $elements.index($currentElement) - countCurrItems,
+            countLeftItems = $elements.index($currentElement),
+            elementPos;
+
+        if($(element).hasClass("js-sldr__dsbl-btn") || $(element).hasClass("js-sldr__dsbl-btn"))
+            return;
+
+        $(element).siblings(".js-sldr__prvs").removeClass("js-sldr__dsbl-btn");
+        $(element).siblings(".js-sldr__next").removeClass("js-sldr__dsbl-btn");
+        
+        if(direction === 'right') {
+                if(countRightItems > countCurrItems) {
+                    $startElement = $elements.eq($elements.index($currentElement) + countCurrItems);   
+                }
+                else {
+                    $startElement = $elements.eq($elements.length - countCurrItems);
+                    $(element).addClass("js-sldr__dsbl-btn");
+                }
+        }
+        else if (direction === 'left') {    
+            if(countLeftItems > countCurrItems) {
+                $startElement = $elements.eq($elements.index($currentElement) - countCurrItems);   
+            }
+            else {
+                $startElement = $elements.eq(0);
+                $(element).addClass("js-sldr__dsbl-btn");
+            }
+        }
+
+        $currentElement.removeClass("js-sldr-crnt");
+        $startElement.addClass("js-sldr-crnt");
+
+        //IE dones not support transitions.
+        elementPos = -$startElement.position().left;
+        if(browserVersion[0]==="MSIE" && browserVersion[1]<10) {
+            $elementWrapper.css({
+                "left" : elementPos
+            });
+        } else {
+            $elementWrapper.css({
+                'transform': 'translateX('+ elementPos +'px)',
+                '-webkit-transform': 'translateX(' + elementPos + 'px)',
+                '-ms-transform': 'translateX(' + elementPos + 'px)'
+            });
+        }
+
+
+        return false;
+    }
+}
+
+var browserVersion = (function(){
+    var ua= navigator.userAgent, tem, 
+    M= ua.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || [];
+    if(/trident/i.test(M[1])){
+        tem=  /\brv[ :]+(\d+)/g.exec(ua) || [];
+        return 'IE '+(tem[1] || '');
+    }
+    if(M[1]=== 'Chrome'){
+        tem= ua.match(/\bOPR\/(\d+)/);
+        if(tem!= null) return 'Opera '+tem[1];
+    }
+    M= M[2]? [M[1], M[2]]: [navigator.appName, navigator.appVersion, '-?'];
+    if((tem= ua.match(/version\/(\d+)/i))!= null) M.splice(1, 1, tem[1]);
+    return M;
+})();
