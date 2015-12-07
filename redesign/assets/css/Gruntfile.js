23610433714fe880789e8beebd32d0c0d8ca191c
SaurabhRaje <saurabh.r@live.com>
2015-12-07 17:30:42 +0530

    Gruntfile Fixes
    - Renamed msp.* files to msp_new.*
    - Fixed map generation for msp_new.css.map
    - Removed unused stripCssComments Node module

--- redesign/assets/css/Gruntfile.js
+++ redesign/assets/css/Gruntfile.js
@@ -1,44 +1,43 @@
 module.exports = function(grunt) {
-	grunt.initConfig({
-		less: {
-			msp: {
-				options: {
-					sourceMap: true,
-					sourceMapFilename: "msp.css.map",
-					sourceMapFilename: "msp_compat.css.map"
-				},
-				files: {
-					"msp.css": "msp.less",
-					"msp_compat.css": "msp_compat.less"
-				}
-			}
-		},
-		stripCssComments: {
-	        dist: {
-	            files: {
-	                'msp.css': 'msp.css',
-	                'msp_compat.css' : 'msp_compat.css'
-	            }
-	        }
-	    },
-		cmq: {
-			options: {
-		      log: false
-		    },
-			msp: {
-				files : {
-					'msp.css': ['msp.css'],
-					'msp_compat.css': ['msp_compat.css']
-				}
-			}
-		},
-		watch: {
-			files: ['*.less'],
-			tasks: ['less:msp', 'cmq:msp'],
-		}
-	});
-	grunt.loadNpmTasks('grunt-contrib-less');
-	grunt.loadNpmTasks('grunt-contrib-watch');
-	grunt.loadNpmTasks('grunt-combine-media-queries');
-	grunt.registerTask('default', ['less:msp', 'cmq:msp', 'watch']);
+  grunt.initConfig({
+    less: {
+      msp_new: {
+        options: {
+          sourceMap: true,
+          sourceMapFilename: "msp_new.css.map"
+        },
+        files: {
+          "msp_new.css": "msp_new.less"
+        }
+      },
+      msp_compat: {
+        options: {
+          sourceMap: true,
+          sourceMapFilename: "msp_compat.css.map"
+        },
+        files: {
+          "msp_compat.css": "msp_compat.less"
+        }
+      }
+    },
+    cmq: {
+      options: {
+        log: false
+      },
+      msp: {
+        files : {
+          "msp_new.css": ["msp_new.css"],
+          "msp_compat.css": ["msp_compat.css"]
+        }
+      }
+    },
+    watch: {
+      files: ["*.less"],
+      tasks: ["less:msp_new", "less:msp_compat", "cmq:msp"]
+    }
+  });
+  grunt.loadNpmTasks("grunt-contrib-less");
+  grunt.loadNpmTasks("grunt-combine-media-queries");
+  grunt.loadNpmTasks("grunt-contrib-watch");
+  grunt.registerTask("default", ["less:msp_new", "less:msp_compat", "cmq:msp", "watch"]);
 }
